03743ad527076c7e74922d7912372de946e2dd96
Soofmax <124178747+Soofmaax@users.noreply.github.com>
2026-08-12 22:49:21 +0200

    Add commitments landing section

--- front-page.php
+++ front-page.php
@@ -6,6 +6,7 @@
     <?php get_template_part( 'template-parts/story-section' ); ?>
     <?php get_template_part( 'template-parts/products-section' ); ?>
     <?php get_template_part( 'template-parts/coffee-journey' ); ?>
+    <?php get_template_part( 'template-parts/commitments-section' ); ?>
 </main>
 
 <?php get_footer(); ?>
